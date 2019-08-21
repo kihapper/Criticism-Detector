b849f4d0401e25c8f5dd822836b2b84df5176185
kihapper <playful.intervention@gmail.com>
2019-08-21 15:20:35 +0200

    update recogniton

--- voice.js
+++ voice.js
@@ -1,103 +1,110 @@
-  
+  window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+  let midtextTag = document.querySelector(".midwayText");
+  let textTag = document.querySelector(".recognitionText");
+
+  var initButton = document.getElementById("Button");
+  initButton.addEventListener("click", startRecognition);
+
+  var currentState = "stopped";
+
   // ML5 —————————
   let prediction;
   let finalScore;
-  console.log("ml5 version:" +  ml5.version);
+  console.log("ml5 version:" + ml5.version);
   const sentiment = ml5.sentiment('movieReviews', modelReady);
 
   // When the model is loaded
   function modelReady() {
-  console.log("Machine Learning Model Loaded!");
+    console.log("Machine Learning Model Loaded!");
   }
 
-  // ——————————————
+  // Bowser to detect browser——————————————
 
-    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-    
-    let midtextTag = document.querySelector(".midwayText");
-    let textTag = document.querySelector(".recognitionText");
+  var result = bowser.getParser(window.navigator.userAgent);
+  console.log(result);
+  console.log("Browser is " + result.parsedResult.browser.name);
 
-    var initButton = document.getElementById("Button");
-    initButton.addEventListener("click", startRecognition);
+  if (result.parsedResult.browser.name !== "Chrome") {
+    alert("Sorry, voice recognition is only available on the latest version of Chrome desktop and is not supported on " + result.parsedResult.browser.name);
+    //initButton.style.display = "none";
+  } else {
+    console.log("This Browser Supports Recognition");
+  }
 
-    var currentState = "stopped";
+  // ———————————————————————————————————————————————
 
 
-    if ('SpeechRecognition' in window) {
-        console.log("This Browser Supports Recognition");
-      } else {
-        alert("Not Supported");
-      }
 
-    //Initiates SpeechRecognition instance
-    let recognition = new window.SpeechRecognition();
-    let finalTranscript = '';
+  //Initiates SpeechRecognition instance
+  let recognition = new window.SpeechRecognition();
+  let finalTranscript = '';
 
-    //Show the results midway when recognizing
-    recognition.interimResults = true;
+  //Show the results midway when recognizing
+  recognition.interimResults = true;
 
-    //Specify Language Here 
-    recognition.lang = "en-GB"
+  //Specify Language Here 
+  recognition.lang = "en-GB"
 
-    //Show how many alternatvies to return
-    recognition.maxAlternatives = 10;
+  //Show how many alternatvies to return
+  recognition.maxAlternatives = 10;
 
-    //Will return sentences continously
-    recognition.continuous = true;
+  //Will return sentences continously
+  recognition.continuous = true;
 
-    function startRecognition() {
-      
-      if(currentState == "stopped"){
-      initButton.innerHTML = "Pause Recognition"
+  function startRecognition() {
+
+    if (currentState == "stopped") {
+      initButton.innerHTML = "Pause Speech Recognition"
       currentState = "recognizing"
       recognition.start();
-      }
-      else if (currentState == "recognizing"){
+      initButton.classList.add("blink");
+    } else if (currentState == "recognizing") {
       initButton.innerHTML = "Start Recognition"
       currentState = "stopped"
-      recognition.stop();
-      }
-    }
-
-
-    recognition.onresult = (event) => {
-      //Inialize interim transcripts
-      let interimTranscript = '';
-
-      //event returns the whole dataset. reuslt Index is the depth of each word
-      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-        let transcript = event.results[i][0].transcript;
-        if (event.results[i].isFinal) {
-
-          //ml5.js to rate the final transcripts score
-          prediction = sentiment.predict(transcript);
-          console.log("IntermediateScore" + prediction.score);
-          finalScore = Math.floor(prediction.score*100);
-          console.log(finalScore + " for : " + transcript);
-
-          finalTranscript =  transcript + '<br>' + finalScore + "/100" + '<br><br>' + finalTranscript ;
-        } else {
-          interimTranscript = interimTranscript + transcript;
-        }
-      }
-      midtextTag.innerHTML = interimTranscript;
-      textTag.innerHTML =  '<span class= "finaltext">' + finalTranscript + '</span>';
-    }
-    
-    recognition.onerror = (event) => {
-        console.log('Speech recognition error detected: ' + event.error);
-      }
-    
-    recognition.onspeechend = ()=> {
-      console.log('Speech has stopped');
+      initButton.classList.remove("blink");
       recognition.stop();
     }
+  }
 
-    
-    recognition.onend = ()=> {
-        console.log('Speech recognition service disconnected');
-        //recognition.start();
+
+  recognition.onresult = (event) => {
+    //Inialize interim transcripts
+    let interimTranscript = '';
+
+    //event returns the whole dataset. reuslt Index is the depth of each word
+    for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+      let transcript = event.results[i][0].transcript;
+      if (event.results[i].isFinal) {
+
+        //ml5.js to rate the final transcripts score
+        prediction = sentiment.predict(transcript);
+        console.log("IntermediateScore" + prediction.score);
+        finalScore = Math.floor(prediction.score * 100);
+        console.log(finalScore + " for : " + transcript);
+
+        finalTranscript = transcript + '<br>' + '<span class= "score">' + "Score: " + finalScore + "/100" + '</span>' + '<br><br>' + finalTranscript;
+      } else {
+        interimTranscript = interimTranscript + transcript;
+      }
     }
-      
+    midtextTag.innerHTML = interimTranscript;
+    textTag.innerHTML = '<span class= "finaltext">' + finalTranscript + '</span>';
+  }
 
-  
+  recognition.onerror = (event) => {
+    console.log('Speech recognition error detected: ' + event.error);
+  }
+
+  recognition.onspeechend = () => {
+    console.log('Speech has stopped');
+    recognition.stop();
+    currentState = "stopped"
+
+  }
+
+  recognition.onend = () => {
+    console.log('Speech recognition service disconnected');
+    currentState = "stopped"
+    startRecognition(); 
+  }
